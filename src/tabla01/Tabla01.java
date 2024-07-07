0762486ef9db2acc0e2e7ea50763dd6107ab443d
joaquingaleano <luchogaleano1998@gmail.com>
2024-07-07 15:59:19 -0300

    Initial commit

---
+++ src/tabla01/Tabla01.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tabla01;
+
+import tabla01.igu.Principal;
+
+
+public class Tabla01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Principal princ = new Principal();
+        princ.setVisible(true);
+        princ.setLocationRelativeTo(null);
+    }
+    
+}
